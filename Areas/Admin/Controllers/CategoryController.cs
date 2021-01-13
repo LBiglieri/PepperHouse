cbe512211b37be5eaca207163f8a95c70bb932e7
LBiglieri <lautarobiglieri@hotmail.com>
2021-01-13 15:47:10 -0300

    Se agregó el controlador de "Category" al area "Admin" con un GET.

---
+++ Areas/Admin/Controllers/CategoryController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PepperHouse.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PepperHouse.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        //GET
+        public async Task<IActionResult> Index()
+        {
+
+            return View(await _db.Category.ToListAsync());
+        }
+    }
+}
