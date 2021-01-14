e877391f81d73fd7fb00659ddc2bba76d94f61b4
LBiglieri <lautarobiglieri@hotmail.com>
2021-01-14 11:52:06 -0300

    Se crearon los Get y Post en el controlador de "Category" para la creación de las categorías.

--- Areas/Admin/Controllers/CategoryController.cs
+++ Areas/Admin/Controllers/CategoryController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using PepperHouse.Data;
+using PepperHouse.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -21,6 +22,27 @@ namespace PepperHouse.Areas.Admin.Controllers
         public async Task<IActionResult> Index()
         {
             return View(await _db.Category.ToListAsync());
+        }
+
+        //GET - Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        //POST - Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Category category)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Category.Add(category);
+                await _db.SaveChangesAsync();
+
+                return RedirectToAction(nameof(Index));
+            }
+            return View(category);
         }
     }
 }
