b5b69c97a04a8df07e88a8bf5066c6c4084684fe
LBiglieri <lautarobiglieri@hotmail.com>
2021-01-18 15:23:46 -0300

    Se creó el modelo de Sub Categoría.

---
+++ Models/SubCategory.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PepperHouse.Models
+{
+    public class SubCategory
+    {
+        [Key]
+        public int ID { get; set; }
+
+        [Display(Name = "SubCategory Name")]
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        [Display(Name ="Category")]
+        public int CategoryID { get; set; }
+
+        [ForeignKey("CategoryID")]
+        public virtual Category Category { get; set; }
+    }
+}
