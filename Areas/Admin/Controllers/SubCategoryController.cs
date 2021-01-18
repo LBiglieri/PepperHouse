430c370e3bfc775b4a69521ffea02501d93bd7f5
LBiglieri <lautarobiglieri@hotmail.com>
2021-01-18 15:39:08 -0300

    Se generó el controlador para el modelo "SubCategory" bajo el área "Admin" con inyección de dependencias y un metodo "Index" que incluye el nombre de la categoría a la que está relacionada.

---
+++ Areas/Admin/Controllers/SubCategoryController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PepperHouse.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PepperHouse.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class SubCategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public SubCategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        //GET - Index
+        public async Task<IActionResult> Index()
+        {
+            var subCategories = await _db.SubCategory.Include(s=>s.Category).ToListAsync();
+            return View(subCategories);
+        }
+    }
+}
