eab7d31a352eed2b3b22f7a574a612016a7ec327
LBiglieri <lautarobiglieri@hotmail.com>
2021-01-20 15:34:34 -0300

    Se añadió el modelo "MenuItem"

---
+++ Data/Migrations/20210120182645_addMenuItemToDB.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PepperHouse.Data.Migrations
+{
+    public partial class addMenuItemToDB : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "MenuItem",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(nullable: false),
+                    Description = table.Column<string>(nullable: true),
+                    Hotness = table.Column<string>(nullable: true),
+                    Image = table.Column<string>(nullable: true),
+                    CategoryID = table.Column<int>(nullable: false),
+                    SubCategoryID = table.Column<int>(nullable: false),
+                    Price = table.Column<double>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MenuItem", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_MenuItem_Category_CategoryID",
+                        column: x => x.CategoryID,
+                        principalTable: "Category",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.NoAction);
+                    table.ForeignKey(
+                        name: "FK_MenuItem_SubCategory_SubCategoryID",
+                        column: x => x.SubCategoryID,
+                        principalTable: "SubCategory",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.NoAction);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_MenuItem_CategoryID",
+                table: "MenuItem",
+                column: "CategoryID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_MenuItem_SubCategoryID",
+                table: "MenuItem",
+                column: "SubCategoryID");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "MenuItem");
+        }
+    }
+}
---
+++ Models/MenuItem.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PepperHouse.Models
+{
+    public class MenuItem
+    {
+        public int ID { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+        public string Hotness { get; set; }
+        public enum EHot { NA=0, Mild=1, Medium=2, Hot=3, VeryHot=4}
+
+        public string Image { get; set; }
+
+        [Display(Name = "Category")]
+        public int CategoryID { get; set; }
+
+        [ForeignKey("CategoryID")]
+        public virtual Category Category { get; set; }
+
+        [Display(Name = "SubCategory")]
+        public int SubCategoryID { get; set; }
+
+        [ForeignKey("SubCategoryID")]
+        public virtual SubCategory SubCategory { get; set; }
+
+        [Range(1,int.MaxValue, ErrorMessage ="Price should be grater than ${1}")]
+        public double Price { get; set; }
+    }
+}
