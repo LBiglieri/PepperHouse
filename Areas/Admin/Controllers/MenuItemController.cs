6deb8d8e5dd152c39dcd12c4072d2f9471a66615
LBiglieri <lautarobiglieri@hotmail.com>
2021-01-21 19:36:46 -0300

    Se creo la acción GET para el metodo Create dentro del MenuItemControler.

--- Areas/Admin/Controllers/MenuItemController.cs
+++ Areas/Admin/Controllers/MenuItemController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using PepperHouse.Data;
+using PepperHouse.Models.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -14,15 +15,29 @@ namespace PepperHouse.Areas.Admin.Controllers
     {
         private readonly ApplicationDbContext _db;
         private readonly IWebHostEnvironment _webHostEnvironment;
+
+        [BindProperty]
+        public MenuItemViewModel MenuItemVM { get; set; }
         public MenuItemController(ApplicationDbContext db, IWebHostEnvironment webHostEnvironment)
         {
             _db = db;
             _webHostEnvironment = webHostEnvironment;
+            MenuItemVM = new MenuItemViewModel()
+            {
+                Category = _db.Category,
+                MenuItem = new Models.MenuItem()
+            };
         }
         public async Task<IActionResult> Index()
         {
             var menuItems = await _db.MenuItem.Include(m=>m.Category).Include(m=>m.SubCategory).ToListAsync();
             return View(menuItems);
+        }
+
+        //GET - Create
+        public IActionResult Create()
+        {
+            return View(MenuItemVM)
         }
     }
 }
