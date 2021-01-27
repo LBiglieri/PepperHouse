49414561a3a65d113c968b8f1baabd91d17ddf7b
LBiglieri <lautarobiglieri@hotmail.com>
2021-01-27 15:28:43 -0300

    Se crearon GET y POST para el metodo Create.

--- Areas/Admin/Controllers/CouponController.cs
+++ Areas/Admin/Controllers/CouponController.cs
@@ -1,8 +1,10 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using PepperHouse.Data;
+using PepperHouse.Models;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -19,6 +21,38 @@ namespace PepperHouse.Areas.Admin.Controllers
         public async Task<IActionResult> Index()
         {
             return View(await _db.Coupon.ToListAsync());
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Coupon coupon)
+        {
+            if (ModelState.IsValid)
+            {
+                var files = HttpContext.Request.Form.Files;
+                if (files.Count > 0)
+                {
+                    byte[] p1 = null;
+                    using (var fs1 = files[0].OpenReadStream())
+                    {
+                        using (var ms1 = new MemoryStream())
+                        {
+                            fs1.CopyTo(ms1);
+                            p1 = ms1.ToArray();
+                        }
+                    }
+                    coupon.Picture = p1;
+                }
+                _db.Coupon.Add(coupon);
+                await _db.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(coupon);
         }
     }
 }
