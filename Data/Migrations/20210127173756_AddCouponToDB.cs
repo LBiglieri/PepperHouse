9c79dc2b96d643297db9b0ab3945537bc998c860
LBiglieri <lautarobiglieri@hotmail.com>
2021-01-27 14:38:48 -0300

    Se añadió modelo y migración para Cupones.

---
+++ Data/Migrations/20210127173756_AddCouponToDB.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PepperHouse.Data.Migrations
+{
+    public partial class AddCouponToDB : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Coupon",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(nullable: false),
+                    CouponType = table.Column<string>(nullable: false),
+                    Discount = table.Column<double>(nullable: false),
+                    MinumumAmount = table.Column<double>(nullable: false),
+                    Picture = table.Column<byte[]>(nullable: true),
+                    IsActive = table.Column<bool>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Coupon", x => x.ID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Coupon");
+        }
+    }
+}
---
+++ Models/Coupon.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PepperHouse.Models
+{
+    public class Coupon
+    {
+        [Key]
+        public int ID { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string CouponType { get; set; }
+        public enum ECouponType { Percent=0, Dollar=1 }
+
+        [Required]
+        public double Discount { get; set; }
+
+        [Required]
+        public double MinumumAmount { get; set; }
+
+        public byte[] Picture { get; set; }
+
+        public bool IsActive { get; set; }
+    }
+}
