3b710bbea99813e551ba7b7024d3fe90acded55b
LBiglieri <lautarobiglieri@hotmail.com>
2021-01-29 15:14:32 -0300

    Se creó un ViewModel para el Menú.

---
+++ Models/ViewModels/MenuViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PepperHouse.Models.ViewModels
+{
+    public class MenuViewModel
+    {
+        public IEnumerable<MenuItem> MenuItem { get; set; }
+        public IEnumerable<Category> Category { get; set; }
+        public IEnumerable<Coupon> Coupon { get; set; }
+
+    }
+}
