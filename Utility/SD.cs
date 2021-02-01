042d5d2bab6805f7971188da19cada9614934586
LBiglieri <lautarobiglieri@hotmail.com>
2021-02-01 17:10:17 -0300

    Se agregaron constantes para referirse a los roles en StaticDetails.

--- Utility/SD.cs
+++ Utility/SD.cs
@@ -7,6 +7,13 @@ namespace PepperHouse.Utility
 {
     public static class SD
     {
+        //static details
         public const string DefaultFoodImage = "default_food.png";
+
+        //roles
+        public const string ManagerUser = "Manager";
+        public const string KitchenUser = "Kitchen";
+        public const string FrontDeskUser = "FrontDesk";
+        public const string CustomerEndUser = "Customer";
     }
 }
