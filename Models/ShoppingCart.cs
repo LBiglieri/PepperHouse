0424adeb1cff5bf2e4832b7080b5bde4031e7f82
LBiglieri <lautarobiglieri@hotmail.com>
2021-02-03 14:54:35 -0300

    Se creó el modelo para el ShoppingCart y se realizó su migración.

---
+++ Models/ShoppingCart.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PepperHouse.Models
+{
+    public class ShoppingCart
+    {
+        public ShoppingCart()
+        {
+            Count = 1;
+        }
+
+        public int ID { get; set; }
+
+        public string ApplicationUserID { get; set; }
+        [NotMapped]
+        [ForeignKey("ApplicationUserID")]
+        public virtual ApplicationUser ApplicationUser { get; set; }
+
+        public int MenuItemID { get; set; }
+        [NotMapped]
+        [ForeignKey("MenuItemID")]
+        public virtual MenuItem MenuItem { get; set; }
+
+        [Range(1,int.MaxValue, ErrorMessage = "Please enter a value greater than or equal to {1}")]
+        public int Count { get; set; }
+    }
+}
