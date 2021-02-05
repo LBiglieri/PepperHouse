9a7f3c8d6126da75da27999d8ea62aecc23ea68c
LBiglieri <lautarobiglieri@hotmail.com>
2021-02-05 16:15:42 -0300

    Se añadió la migración de los modelos OrderDetails y OrderHeader.

---
+++ Data/Migrations/20210205191325_AddOrderHeaderAndDetailsToDB.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PepperHouse.Data.Migrations
+{
+    public partial class AddOrderHeaderAndDetailsToDB : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "OrderHeader",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserID = table.Column<string>(nullable: false),
+                    OrderDate = table.Column<DateTime>(nullable: false),
+                    OrderTotalOriginal = table.Column<double>(nullable: false),
+                    OrderTotal = table.Column<double>(nullable: false),
+                    PickupTime = table.Column<DateTime>(nullable: false),
+                    CouponCode = table.Column<string>(nullable: true),
+                    CouponCodeDiscount = table.Column<double>(nullable: false),
+                    Status = table.Column<string>(nullable: true),
+                    PaymentStatus = table.Column<string>(nullable: true),
+                    Comments = table.Column<string>(nullable: true),
+                    PickupName = table.Column<string>(nullable: true),
+                    PhoneNumber = table.Column<string>(nullable: true),
+                    TransactionID = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_OrderHeader", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_OrderHeader_AspNetUsers_UserID",
+                        column: x => x.UserID,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "OrderDetails",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    OrderID = table.Column<int>(nullable: false),
+                    MenuItemID = table.Column<int>(nullable: false),
+                    Count = table.Column<int>(nullable: false),
+                    Name = table.Column<string>(nullable: true),
+                    Description = table.Column<string>(nullable: true),
+                    Price = table.Column<double>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_OrderDetails", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_OrderDetails_MenuItem_MenuItemID",
+                        column: x => x.MenuItemID,
+                        principalTable: "MenuItem",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_OrderDetails_OrderHeader_OrderID",
+                        column: x => x.OrderID,
+                        principalTable: "OrderHeader",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_OrderDetails_MenuItemID",
+                table: "OrderDetails",
+                column: "MenuItemID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_OrderDetails_OrderID",
+                table: "OrderDetails",
+                column: "OrderID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_OrderHeader_UserID",
+                table: "OrderHeader",
+                column: "UserID");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "OrderDetails");
+
+            migrationBuilder.DropTable(
+                name: "OrderHeader");
+        }
+    }
+}
