8d22f6ba46f47c6143c823ad5c21135067d1fd07
LBiglieri <lautarobiglieri@hotmail.com>
2021-02-05 16:15:00 -0300

    Se crearon los modelos para OrderDetails y OrderHeader.

---
+++ Models/OrderDetails.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PepperHouse.Models
+{
+    public class OrderDetails
+    {
+        [Key]
+        public int ID { get; set; }
+
+        [Required]
+        public int OrderID { get; set; }
+        [ForeignKey("OrderID")]
+        public virtual OrderHeader OrderHeader { get; set; }
+
+        [Required]
+        public int MenuItemID { get; set; }
+        [ForeignKey("MenuItemID")]
+        public virtual MenuItem MenuItem { get; set; }
+
+        public int Count { get; set; }
+
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+        [Required]
+        public double Price { get; set; }
+    }
+}
---
+++ Models/OrderHeader.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PepperHouse.Models
+{
+    public class OrderHeader
+    {
+        [Key]
+        public int ID { get; set; }
+
+        [Required]
+        public string UserID { get; set; }
+
+        [ForeignKey("UserID")]
+        public virtual ApplicationUser ApplicationUser { get; set; }
+
+        [Required]
+        public DateTime OrderDate { get; set; }
+
+        [Required]
+        public double OrderTotalOriginal { get; set; }
+
+        [Required]
+        [DisplayFormat(DataFormatString = "{0:C}")]
+        [Display(Name ="Order Total")]
+        public double OrderTotal { get; set; }
+
+        [Required]
+        [Display(Name ="Pickup Time")]
+        public DateTime PickupTime { get; set; }
+
+        [Required]
+        [NotMapped]
+        public DateTime PickUpDate { get; set; }
+
+        [Display(Name = "Coupon Code")]
+        public string CouponCode { get; set; }
+        public double CouponCodeDiscount { get; set; }
+        public string Status{ get; set; }
+        public string PaymentStatus { get; set; }
+        public string Comments { get; set; }
+
+        [Display(Name ="Pickup Name")]
+        public string PickupName { get; set; }
+
+        [Display(Name = "Phone Number")]
+        public string PhoneNumber { get; set; }
+
+        public string TransactionID { get; set; }
+    }
+}
