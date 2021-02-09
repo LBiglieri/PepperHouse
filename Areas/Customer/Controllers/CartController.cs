d07686b67789b1301607a4a549d182c382a2a116
LBiglieri <lautarobiglieri@hotmail.com>
2021-02-09 19:55:29 -0300

    Se agregó el método para agregar y quitar cupones.

--- Areas/Customer/Controllers/CartController.cs
+++ Areas/Customer/Controllers/CartController.cs
@@ -69,5 +69,13 @@ namespace PepperHouse.Areas.Customer.Controllers
 
             return RedirectToAction(nameof(Index));
         }
+
+        public IActionResult RemoveCoupon()
+        {
+
+            HttpContext.Session.SetString(SD.ssCouponCode, string.Empty);
+
+            return RedirectToAction(nameof(Index));
+        }
     }
 }
