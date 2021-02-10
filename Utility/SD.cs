78eba73696506d1daea4553e9b181f2966bad69c
LBiglieri <lautarobiglieri@hotmail.com>
2021-02-10 19:13:03 -0300

    Se agregaron varias strings para señalar estado de orden y estado de pago.

--- Utility/SD.cs
+++ Utility/SD.cs
@@ -13,6 +13,16 @@ namespace PepperHouse.Utility
         public const string ssShoppingCartCount = "ssCartCount";
         public const string ssCouponCode = "ssCouponCode";
 
+        public const string StatusSubmitted = "Submitted";
+        public const string StatusInProcess = "Being Prepared";
+        public const string StatusReady = "Ready for Pickup";
+        public const string StatusCompleted = "Completed";
+        public const string StatusCancelled = "Cancelled";
+
+        public const string PaymentStatusPending = "Pending";
+        public const string PaymentStatusApproved = "Approved";
+        public const string PaymentStatusRejected = "Rejected";
+
         //roles
         public const string ManagerUser = "Manager";
         public const string KitchenUser = "Kitchen";
