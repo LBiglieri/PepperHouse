d6c017baf4dfa01c90be2b90db10fcfa3732c16f
LBiglieri <lautarobiglieri@hotmail.com>
2021-02-19 11:49:25 -0300

    Se creó un nuevo ViewComponent para manejar ApplicationUser´s.

---
+++ ViewComponents/UserNameViewComponent.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PepperHouse.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace PepperHouse.ViewComponents
+{
+    public class UserNameViewComponent : ViewComponent
+    {
+        private readonly ApplicationDbContext _db;
+        public UserNameViewComponent(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var claimsIdentity = (ClaimsIdentity)User.Identity;
+            var claims = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
+
+            var userFromDB = await _db.ApplicationUser.FirstOrDefaultAsync(u => u.Id == claims.Value);
+
+            return View(userFromDB);
+        }
+    }
+}
